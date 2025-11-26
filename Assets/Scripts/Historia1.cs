077a978646f4e3743d740a8733858d25f2c17d2a
Mariana <marianaoazevedo03@gmail.com>
2025-11-26 19:07:25 -0300

    subindo os arquivos que faltaram no commit anterior

--- Assets/Scripts/Historia1.cs
+++ Assets/Scripts/Historia1.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Historia1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("CenaHistoria2");
     }
 }
